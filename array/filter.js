315762caebadaab3ac9d2d2e505ebe7b4f7319b8
SAJAN MAINALI <sajanrajmainali99@gmail.com>
2022-03-16 22:41:20 +0545

    filter array function

---
+++ array/filter.js
@@ -0,0 +1,19 @@
+
+//Using filter array function
+
+
+let number=[3,7,9,3,6,6,78,5665,75,4,54,78];
+
+// filtering the number which are divisible by 5
+
+
+
+output=number.filter(function divisible(x)
+
+{
+
+    return x%2==0 ;
+
+});
+console.log(output);
+
