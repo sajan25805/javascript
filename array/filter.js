2ea820af1c9d8acd29e88a343f30f319da1ae536
SAJAN MAINALI <sajanrajmainali99@gmail.com>
2022-03-18 11:23:31 +0545

    Array function reuce is added

--- array/filter.js
+++ array/filter.js
@@ -2,18 +2,81 @@
 //Using filter array function
 
 
-let number=[3,7,9,3,6,6,78,5665,75,4,54,78];
+// let number=[3,7,9,3,6,6,78,5665,75,4,54,78];
 
 // filtering the number which are divisible by 5
+// First way
 
 
 
-output=number.filter(function divisible(x)
+// output=number.filter(function divisible(x)
 
+// {
+
+//     return x%5==0 ;
+
+// });
+
+// console.log(output);
+
+// Using arrow function
+
+// output=number.filter((x)=> x%5==0);
+// console.log(output);
+
+// Display only the elements in array which are greater than 
+
+// output=number.filter((x)=>x>2);
+
+//  function greaterThan(x)
+//  {
+//      return x>200;
+//  }
+// output=number.filter(greaterThan)
+// console.log(output);
+
+
+let number= [1,2,3,4,5,6,7,8,9,10]
+ 
+let output=number.filter((x)=>
 {
-
-    return x%2==0 ;
-
+    return x>2;
 });
 console.log(output);
+// Find the sum of all the elements in the array.
+
+// arr= [1,2,3,4,5,6,7,8,9];
+// let output= function(arr)
+// {
+//     let sum=0;
+//     for (let i = 0; i < arr.length; i++)
+//      {
+//         sum = sum + arr[i];
+        
+//     }
+//     return sum;
+// }
+//  console.log(output(arr))
+
+// FInd the sum of the element in the array using arrow function
+
+// arr=[1,2,3,4,5,6]
+
+// let output= (arr)=>
+// {
+// let sum=0;
+
+// for (let i = 0; i < arr.length; i++)
+//  {
+//     sum = sum +arr[i];
+    
+// }
+//  return sum;
+// }
+
+// console.log(output(arr));
+
+
+
+
 
