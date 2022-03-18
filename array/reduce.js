a9e4d3890cee005fa7ec13707c48fcc4537b2d6d
SAJAN MAINALI <sajanrajmainali99@gmail.com>
2022-03-18 12:51:44 +0545

    Reduce new program done

--- array/reduce.js
+++ array/reduce.js
@@ -27,24 +27,27 @@
 console.log(output);
 
 
+//3. Find the maximum element of the array without using reduce.
+// output=(number)=>{
+//     let max=0;
+//     for (let i = 0; i < number.length; i++) 
+//     {
+//     if(number[i]>max)
+//     {
+//         max=number[i];
+//     } }
+//     return max;
+// };
+//  console.log(output(number));
 
 
+// 4. Find the maximum, element of the array using reduce.
 
+outputt = number.reduce (function (max, crr) {
+        if (crr > max) {
+            max = crr;
+        }
+        return max;
+    },0);
+console.log(outputt);
 
-// FInd the sum of the element in the array using arrow function
-
-// arr=[1,2,3,4,5,6]
-
-// let output= (arr)=>
-// {
-// let sum=0;
-
-// for (let i = 0; i < arr.length; i++)
-//  {
-//     sum = sum +arr[i];
-    
-// }
-//  return sum;
-// }
-
-// console.log(output(arr));
